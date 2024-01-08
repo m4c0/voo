fb7165e6fea99e2a2685901a2cbb639c176caa40
Eduardo Costa <m4c0@users.noreply.github.com>
2024-01-08 16:28:40 +0000

    add h2l_buffer to poc

--- poc.cpp
+++ poc.cpp
@@ -6,6 +6,10 @@ import casein;
 import vee;
 import voo;
 
+struct inst {
+  float x, y;
+};
+
 class thread : public voo::casein_thread {
 public:
   void run() override {
@@ -14,8 +18,12 @@ public:
     voo::one_quad quad{dq};
     vee::command_buffer cb =
         vee::allocate_primary_command_buffer(dq.command_pool());
+    vee::command_buffer cb1 =
+        vee::allocate_primary_command_buffer(dq.command_pool());
 
     vee::pipeline_layout pl = vee::create_pipeline_layout();
+
+    voo::h2l_buffer insts{dq, 2 * sizeof(inst)};
 
     while (!interrupted()) {
       voo::swapchain_and_stuff sw{dq};
@@ -29,17 +37,28 @@ public:
           },
           .bindings{
               quad.vertex_input_bind(),
+              vee::vertex_input_bind_per_instance(sizeof(inst)),
           },
           .attributes{
               quad.vertex_attribute(0),
+              vee::vertex_attribute_vec2(1, 0),
           },
       });
 
+      {
+        auto m = insts.mapmem();
+        *static_cast<inst *>(*m) = {-0.5, -0.5};
+      }
+
       extent_loop([&] {
         sw.acquire_next_image();
+
+        insts.submit(cb1, dq);
+
         sw.one_time_submit(dq, cb, [&](auto &pcb) {
           auto scb = sw.cmd_render_pass(pcb);
           vee::cmd_bind_gr_pipeline(*scb, *gp);
+          insts.cmd_bind_vertex_buffer(scb, 1);
           quad.run(scb, 0);
         });
         sw.queue_present(dq);
