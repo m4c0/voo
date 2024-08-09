f0b4d789976ee73bb5ed5796492b2a413a8c7b4c
Eduardo Costa <m4c0@users.noreply.github.com>
2024-08-09 21:50:03 +0100

    casein_thread: capture "this" and avoid live resize

--- casein_thread.cpp
+++ casein_thread.cpp
@@ -2,11 +2,8 @@ module voo;
 import casein;
 import sith;
 
-static voo::casein_thread *t{};
-
 voo::casein_thread::casein_thread() {
-  t = this;
-  casein::handle(casein::CREATE_WINDOW, [] { t->m_run = sith::run_guard{t}; });
-  casein::handle(casein::RESIZE_WINDOW, [] { t->m_resized = true; });
-  casein::handle(casein::QUIT, [] { t->m_run = {}; });
+  casein::handle(casein::CREATE_WINDOW, [this] { m_run = sith::run_guard{this}; });
+  casein::handle(casein::RESIZE_WINDOW, [this] { if (!casein::window_live_resize) m_resized = true; });
+  casein::handle(casein::QUIT, [this] { m_run = {}; });
 }
