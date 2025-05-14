3533d7eee1bd897497b3c3bd6d50c4b7e1afe834
Eduardo Costa <m4c0@users.noreply.github.com>
2025-05-14 22:13:00 +0100

    poc: loading image

--- poc-imageload.cpp
+++ poc-imageload.cpp
@@ -1,7 +1,7 @@
 #pragma leco tool
 
 import hai;
-import jojo;
+import stubby;
 import voo;
 
 int main() {
@@ -11,18 +11,19 @@ int main() {
 
   struct bits {
     vee::command_buffer cb;
-    voo::fence f {{}};
+    voo::fence f { voo::fence::signaled {} };
     voo::queue * q;
   } data {
     .cb = cpool.allocate_primary_command_buffer(),
     .q  = dq.queue(),
   };
-  jojo::read("poc-imageload.png", &data, [](auto ptr, hai::array<char> & img) {
+  stbi::load_from_resource("poc-imageload.png", &data, [](auto ptr, auto & img) {
     bits * b = static_cast<bits *>(ptr);
 
     b->f.wait_and_reset();
 
-    voo::cmd_buf_one_time_submit::build(b->cb, [](auto cb) {});
+    voo::cmd_buf_one_time_submit::build(b->cb, [](auto cb) {
+    });
     b->q->queue_submit({
       .fence = b->f,
       .command_buffer = b->cb,
