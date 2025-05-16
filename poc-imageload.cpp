e5ee64d08d597b9c3e1e50cab33ebd8287ae999f
Eduardo Costa <m4c0@users.noreply.github.com>
2025-05-16 22:34:57 +0100

    poc: potentially reusable class

--- poc-imageload.cpp
+++ poc-imageload.cpp
@@ -1,17 +1,19 @@
 #pragma leco tool
 
 import hai;
+import jute;
+import silog;
 import stubby;
 import voo;
 
-int main() {
-  voo::device_and_queue dq { "poc-imageload" };
+struct bound_image {
+  vee::image img;
+  vee::device_memory mem;
+  vee::image_view iv;
+};
 
-  vee::image m_img;
-  vee::device_memory m_mem;
-  vee::image_view m_iv;
-
-  stbi::load("poc-imageload.png", nullptr, [&](auto ptr, auto & img) {
+void load_image(jute::view name, voo::device_and_queue & dq, bound_image & bi, auto && callback) {
+  stbi::load(name, nullptr, [&](auto ptr, auto & img) {
     unsigned w = img.width;
     unsigned h = img.height;
     unsigned sz = w * h * 4;
@@ -23,19 +25,19 @@ int main() {
 
     constexpr const auto fmt = VK_FORMAT_R8G8B8A8_SRGB;
     vee::extent ext { w, h };
-    m_img = vee::create_image(ext, fmt);
-    m_mem = vee::create_local_image_memory(dq.physical_device(), *m_img);
-    vee::bind_image_memory(*m_img, *m_mem);
-    m_iv = vee::create_image_view(*m_img, fmt);
+    bi.img = vee::create_image(ext, fmt);
+    bi.mem = vee::create_local_image_memory(dq.physical_device(), *bi.img);
+    vee::bind_image_memory(*bi.img, *bi.mem);
+    bi.iv = vee::create_image_view(*bi.img, fmt);
 
     voo::fence f { false };
     auto cpool = dq.queue()->create_command_pool();
     auto cb = cpool.allocate_primary_command_buffer();
 
     voo::cmd_buf_one_time_submit::build(cb, [&](auto cb) {
-      vee::cmd_pipeline_barrier(cb, *m_img, vee::from_host_to_transfer);
-      vee::cmd_copy_buffer_to_image(cb, ext, host.buffer(), *m_img);
-      vee::cmd_pipeline_barrier(cb, *m_img, vee::from_transfer_to_fragment);
+      vee::cmd_pipeline_barrier(cb, *bi.img, vee::from_host_to_transfer);
+      vee::cmd_copy_buffer_to_image(cb, ext, host.buffer(), *bi.img);
+      vee::cmd_pipeline_barrier(cb, *bi.img, vee::from_transfer_to_fragment);
     });
     dq.queue()->queue_submit({
       .fence = f,
@@ -43,6 +45,16 @@ int main() {
     });
 
     f.wait();
+    callback();
+  });
+}
+
+int main() {
+  voo::device_and_queue dq { "poc-imageload" };
+
+  bound_image img {};
+  load_image("poc-imageload.png", dq, img, [] {
+    silog::log(silog::info, "Image loaded");
   });
 
   vee::device_wait_idle();
