a4e444fddf8387b47bc77a0cecd65e76dcceb4f6
Eduardo Costa <m4c0@users.noreply.github.com>
2025-05-18 12:21:12 +0100

    poc: nit

--- poc-imageload.cpp
+++ poc-imageload.cpp
@@ -12,12 +12,12 @@ struct bound_image {
   vee::image_view iv;
 };
 
-void load_image(jute::view name, voo::device_and_queue & dq, bound_image & bi, auto && callback) {
+void load_image(jute::view name, vee::physical_device pd, voo::queue * q, bound_image & bi, auto && callback) {
   stbi::load(name, nullptr, [&](auto ptr, auto & img) {
     unsigned w = img.width;
     unsigned h = img.height;
     unsigned sz = w * h * 4;
-    voo::host_buffer host { dq.physical_device(), sz };
+    voo::host_buffer host { pd, sz };
     {
       voo::memiter<unsigned char> c { host.memory() };
       for (auto i = 0; i < sz; i++) c[i] = (*img.data)[i];
@@ -26,12 +26,12 @@ void load_image(jute::view name, voo::device_and_queue & dq, bound_image & bi, a
     constexpr const auto fmt = VK_FORMAT_R8G8B8A8_SRGB;
     vee::extent ext { w, h };
     bi.img = vee::create_image(ext, fmt);
-    bi.mem = vee::create_local_image_memory(dq.physical_device(), *bi.img);
+    bi.mem = vee::create_local_image_memory(pd, *bi.img);
     vee::bind_image_memory(*bi.img, *bi.mem);
     bi.iv = vee::create_image_view(*bi.img, fmt);
 
     voo::fence f { false };
-    auto cpool = dq.queue()->create_command_pool();
+    auto cpool = q->create_command_pool();
     auto cb = cpool.allocate_primary_command_buffer();
 
     voo::cmd_buf_one_time_submit::build(cb, [&](auto cb) {
@@ -39,7 +39,7 @@ void load_image(jute::view name, voo::device_and_queue & dq, bound_image & bi, a
       vee::cmd_copy_buffer_to_image(cb, ext, host.buffer(), *bi.img);
       vee::cmd_pipeline_barrier(cb, *bi.img, vee::from_transfer_to_fragment);
     });
-    dq.queue()->queue_submit({
+    q->queue_submit({
       .fence = f,
       .command_buffer = cb,
     });
@@ -53,7 +53,7 @@ int main() {
   voo::device_and_queue dq { "poc-imageload" };
 
   bound_image img {};
-  load_image("poc-imageload.png", dq, img, [] {
+  load_image("poc-imageload.png", dq.physical_device(), dq.queue(), img, [] {
     silog::log(silog::info, "Image loaded");
   });
 
