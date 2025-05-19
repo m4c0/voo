9f489f1adf60bb943cc2c024bb2f32582db44843
Eduardo Costa <m4c0@users.noreply.github.com>
2025-05-19 01:51:11 +0100

    loading bound_images

--- poc-imageload.cpp
+++ poc-imageload.cpp
@@ -6,48 +6,11 @@ import silog;
 import stubby;
 import voo;
 
-void load_image(jute::view name, vee::physical_device pd, voo::queue * q, voo::bound_image & bi, auto && callback) {
-  stbi::load(name, nullptr, [&](auto ptr, auto & img) {
-    unsigned w = img.width;
-    unsigned h = img.height;
-    unsigned sz = w * h * 4;
-    voo::host_buffer host { pd, sz };
-    {
-      voo::memiter<unsigned char> c { host.memory() };
-      for (auto i = 0; i < sz; i++) c[i] = (*img.data)[i];
-    }
-
-    constexpr const auto fmt = VK_FORMAT_R8G8B8A8_SRGB;
-    vee::extent ext { w, h };
-    bi.img = vee::create_image(ext, fmt);
-    bi.mem = vee::create_local_image_memory(pd, *bi.img);
-    vee::bind_image_memory(*bi.img, *bi.mem);
-    bi.iv = vee::create_image_view(*bi.img, fmt);
-
-    voo::fence f { false };
-    auto cpool = q->create_command_pool();
-    auto cb = cpool.allocate_primary_command_buffer();
-
-    voo::cmd_buf_one_time_submit::build(cb, [&](auto cb) {
-      vee::cmd_pipeline_barrier(cb, *bi.img, vee::from_host_to_transfer);
-      vee::cmd_copy_buffer_to_image(cb, ext, host.buffer(), *bi.img);
-      vee::cmd_pipeline_barrier(cb, *bi.img, vee::from_transfer_to_fragment);
-    });
-    q->queue_submit({
-      .fence = f,
-      .command_buffer = cb,
-    });
-
-    f.wait();
-    callback();
-  });
-}
-
 int main() {
   voo::device_and_queue dq { "poc-imageload" };
 
   voo::bound_image img {};
-  load_image("poc-imageload.png", dq.physical_device(), dq.queue(), img, [] {
+  voo::load_image("poc-imageload.png", dq.physical_device(), dq.queue(), img, [] {
     silog::log(silog::info, "Image loaded");
   });
 
--- voo-sires_image.cppm
+++ voo-sires_image.cppm
@@ -75,4 +75,41 @@ export auto load_sires_image(jute::view file, vee::physical_device pd) {
 export constexpr auto load_sires_image(jute::view file) {
   return [=](vee::physical_device pd) { return load_sires_image(file, pd); };
 }
+
+  export void load_image(jute::view name, vee::physical_device pd, queue * q, voo::bound_image & bi, auto && callback) {
+    stbi::load(name, nullptr, [&](auto ptr, auto & img) {
+      unsigned w = img.width;
+      unsigned h = img.height;
+      unsigned sz = w * h * 4;
+      host_buffer host { pd, sz };
+      {
+        memiter<unsigned char> c { host.memory() };
+        for (auto i = 0; i < sz; i++) c[i] = (*img.data)[i];
+      }
+  
+      constexpr const auto fmt = VK_FORMAT_R8G8B8A8_SRGB;
+      vee::extent ext { w, h };
+      bi.img = vee::create_image(ext, fmt);
+      bi.mem = vee::create_local_image_memory(pd, *bi.img);
+      vee::bind_image_memory(*bi.img, *bi.mem);
+      bi.iv = vee::create_image_view(*bi.img, fmt);
+  
+      fence f { false };
+      auto cpool = q->create_command_pool();
+      auto cb = cpool.allocate_primary_command_buffer();
+  
+      cmd_buf_one_time_submit::build(cb, [&](auto cb) {
+        vee::cmd_pipeline_barrier(cb, *bi.img, vee::from_host_to_transfer);
+        vee::cmd_copy_buffer_to_image(cb, ext, host.buffer(), *bi.img);
+        vee::cmd_pipeline_barrier(cb, *bi.img, vee::from_transfer_to_fragment);
+      });
+      q->queue_submit({
+        .fence = f,
+        .command_buffer = cb,
+      });
+  
+      f.wait();
+      callback();
+    });
+  }
 } // namespace voo
